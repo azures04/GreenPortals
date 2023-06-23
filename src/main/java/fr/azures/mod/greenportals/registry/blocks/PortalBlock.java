e0058727fc0a44f260577cf64ff2dadf93792589
azures04 <gilleslazure04@gmail.com>
2023-06-23 17:48:05 +0200

    commit

--- src/main/java/fr/azures/mod/greenportals/registry/blocks/PortalBlock.java
+++ src/main/java/fr/azures/mod/greenportals/registry/blocks/PortalBlock.java
@@ -3,13 +3,10 @@ package fr.azures.mod.greenportals.registry.blocks;
 import fr.azures.mod.greenportals.GreenPortals;
 import fr.azures.mod.libs.nomorenbt.common.Data;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 public class PortalBlock extends Block {
@@ -22,13 +19,15 @@ public class PortalBlock extends Block {
 	
 	@Override
 	public void stepOn(World worldIn, BlockPos posIn, Entity entityIn) {
-		Data blockData = GreenPortals.getInstance().blocks.getData(mc.level.dimension().toString(), posIn);
+		Data blockData = GreenPortals.getInstance().blocks.getData("minecraft:overworld", posIn);
+		System.out.println(posIn);
 		if (mc.player != null) {
 			try {
 				mc.player.sendMessage(new StringTextComponent("dimX  > " + blockData.getInt("dimX")), mc.player.getUUID());
 				mc.player.sendMessage(new StringTextComponent("dimY  > " + blockData.getInt("dimY")), mc.player.getUUID());
 				mc.player.sendMessage(new StringTextComponent("dimZ  > " + blockData.getInt("dimZ")), mc.player.getUUID());
 				mc.player.sendMessage(new StringTextComponent("dimId > " + blockData.getString("dimId")), mc.player.getUUID());
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
--- src/main/java/fr/azures/mod/greenportals/registry/items/PortalGun.java
+++ src/main/java/fr/azures/mod/greenportals/registry/items/PortalGun.java
@@ -29,29 +29,30 @@ public class PortalGun extends Item {
 	public ActionResult<ItemStack> use(World world, PlayerEntity entity, Hand hand) {
 		BlockPos inPortal = new BlockPos(entity.pick(10.0D, 10.0F, false).getLocation());
 		BlockPos outPortal = new BlockPos(27, 87, -27);
-		Data inPortalData = new Data();
-		inPortalData.putString("dimId", mc.level.dimension().toString());
-    	inPortalData.putInt("dimX", outPortal.getX());
-    	inPortalData.putInt("dimY", outPortal.getY());
-    	inPortalData.putInt("dimZ", outPortal.getZ());
-		GreenPortals.getInstance().blocks.storeData(mc.level.dimension().toString(), inPortal, inPortalData);
 		if (world.getBlockState(inPortal).getBlock() == Blocks.WATER || world.getBlockState(inPortal).getBlock() == Blocks.AIR) {
 			world.setBlock(inPortal, ModBlocks.PORTAL_BLOCK.get().defaultBlockState(), 1);
 			Set<RegistryKey<World>> dimensions = ServerLifecycleHooks.getCurrentServer().levelKeys();
 		        dimensions.forEach(dimension -> {
-		        if ("minecraft:overworld".contains(dimension.location().toString())) {
+		        if (dimension.location().toString().contains(dimension.location().toString())) {
 		        	try {
-			        	//ServerLifecycleHooks.getCurrentServer().getLevel(dimension).setChunkForced(outPortal.getX(), outPortal.getZ(), true);
-			        	ServerLifecycleHooks.getCurrentServer().getLevel(dimension).setBlock(outPortal, ModBlocks.PORTAL_BLOCK.get().defaultBlockState(), 1);
-					} catch (Exception e) {
+			        	ServerLifecycleHooks.getCurrentServer().getLevel(dimension).setChunkForced(outPortal.getX(), outPortal.getZ(), true);
+			        	ServerLifecycleHooks.getCurrentServer().getLevel(dimension).setBlock(outPortal, Blocks.WATER.defaultBlockState(), 0);
+			        	ServerLifecycleHooks.getCurrentServer().getLevel(dimension).setBlock(outPortal, ModBlocks.PORTAL_BLOCK.get().defaultBlockState(), 0);
+		        	} catch (Exception e) {
 						e.printStackTrace();
 					}
+		    		Data inPortalData = new Data();
+		    		inPortalData.putString("dimId", dimension.location().toString());
+		        	inPortalData.putInt("dimX", outPortal.getX());
+		        	inPortalData.putInt("dimY", outPortal.getY());
+		        	inPortalData.putInt("dimZ", outPortal.getZ());
 		        	Data outPortalData = new Data();
-		        	outPortalData.putString("dimId", mc.level.dimension().toString());
-		        	outPortalData.putInt("dimX", outPortal.getX());
-		        	outPortalData.putInt("dimY", outPortal.getY());
-		        	outPortalData.putInt("dimZ", outPortal.getZ());
+		        	outPortalData.putString("dimId", mc.level.dimension().location().toString());
+		        	outPortalData.putInt("dimX", inPortal.getX());
+		        	outPortalData.putInt("dimY", inPortal.getY());
+		        	outPortalData.putInt("dimZ", inPortal.getZ());
 		    		GreenPortals.getInstance().blocks.storeData(dimension.location().toString(), outPortal, outPortalData);
+		    		GreenPortals.getInstance().blocks.storeData(mc.level.dimension().location().toString(), inPortal, inPortalData);
 		        }
 		    });
 		}
