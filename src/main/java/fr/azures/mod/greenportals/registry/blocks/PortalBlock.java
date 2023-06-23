1080b00c3ce5c375a82d234315fdc119ce0c43e3
azures04 <gilleslazure04@gmail.com>
2023-06-23 15:23:29 +0200

    Update PortalBlock.java

--- src/main/java/fr/azures/mod/greenportals/registry/blocks/PortalBlock.java
+++ src/main/java/fr/azures/mod/greenportals/registry/blocks/PortalBlock.java
@@ -1,7 +1,8 @@
 package fr.azures.mod.greenportals.registry.blocks;
 
+import fr.azures.mod.greenportals.GreenPortals;
 import fr.azures.mod.greenportals.registry.ModTilesEntities;
-import fr.azures.mod.greenportals.registry.tileentity.PortalTile;
+import fr.azures.mod.libs.nomorenbt.common.Data;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
@@ -14,20 +15,20 @@ import net.minecraft.world.World;
 
 public class PortalBlock extends Block {
 
+	private Minecraft mc = Minecraft.getInstance();
+
 	public PortalBlock(Properties properties) {
 		super(properties);
 	}
 	
 	@Override
 	public void stepOn(World worldIn, BlockPos posIn, Entity entityIn) {
-		/*Set<RegistryKey<World>> dimensions = ServerLifecycleHooks.getCurrentServer().levelKeys();
-	    dimensions.forEach(dimension -> {
-	        if ("minecraft:the_nether".contains(dimension.location().toString())) {
-	            TeleporterUtils.teleport(entityIn, ServerLifecycleHooks.getCurrentServer().getLevel(dimension), posIn.getX(), posIn.getY(), posIn.getZ(), entityIn.yRot, entityIn.xRot);
-	        }
-	    });*/
-		if (Minecraft.getInstance().player != null) {
-			Minecraft.getInstance().player.sendMessage(new StringTextComponent(worldIn.getBlockEntity(posIn).serializeNBT().toString()), Minecraft.getInstance().player.getUUID());
+		Data blockData = GreenPortals.getInstance().blocks.getData(mc.level.dimension().toString(), posIn);
+		if (mc.player != null) {
+			mc.player.sendMessage(new StringTextComponent("dimX  > " + blockData.getInt("dimX")), mc.player.getUUID());
+			mc.player.sendMessage(new StringTextComponent("dimY  > " + blockData.getInt("dimY")), mc.player.getUUID());
+			mc.player.sendMessage(new StringTextComponent("dimZ  > " + blockData.getInt("dimZ")), mc.player.getUUID());
+			mc.player.sendMessage(new StringTextComponent("dimId > " + blockData.getInt("dimId")), mc.player.getUUID());
 		}
 		super.stepOn(worldIn, posIn, entityIn);
 	}
