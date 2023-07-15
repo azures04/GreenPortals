0cc450275653854b15396120f7ff10878f096709
Azures <gilleslazure04@gmail.com>
2023-07-15 17:23:12 +0200

    new commit

--- src/main/java/fr/azures/mod/greenportals/GreenPortals.java
+++ src/main/java/fr/azures/mod/greenportals/GreenPortals.java
@@ -1,15 +1,19 @@
 package fr.azures.mod.greenportals;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 import org.apache.commons.lang3.tuple.Pair;
 
 import fr.azures.mod.greenportals.registry.ModBlocks;
 import fr.azures.mod.greenportals.registry.ModItems;
-import fr.azures.mod.greenportals.ui.PortalGunConfigScreenUI;
 import fr.azures.mod.greenportals.utils.Constants;
 import fr.azures.mod.libs.nomorenbt.client.LocalStorage;
 import fr.azures.mod.libs.nomorenbt.client.LocalStorage.Blocks;
 import fr.azures.mod.libs.nomorenbt.client.LocalStorage.Items;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.MainMenuScreen;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -27,7 +31,7 @@ public class GreenPortals {
 	public Items items;
 	private static GreenPortals instance;
 	
-	public GreenPortals() {
+	public GreenPortals() throws Exception {
 		this.localStorage = new LocalStorage();
 		this.blocks = localStorage.blocks;
 		this.items = localStorage.items;
--- src/main/java/fr/azures/mod/greenportals/registry/blocks/PortalBlock.java
+++ src/main/java/fr/azures/mod/greenportals/registry/blocks/PortalBlock.java
@@ -1,5 +1,6 @@
 package fr.azures.mod.greenportals.registry.blocks;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import fr.azures.mod.greenportals.GreenPortals;
@@ -9,8 +10,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
@@ -29,6 +34,7 @@ public class PortalBlock extends Block {
 	
 	@Override 
 	public void entityInside(BlockState state, World worldIn, BlockPos posIn, Entity entityIn) {
+		System.out.println("a");
 		Data blockData = GreenPortals.getInstance().blocks.getData(mc.getLevelSource().getBaseDir().toString(), posIn);
 		if (mc.player != null) {
 			try {
@@ -39,6 +45,7 @@ public class PortalBlock extends Block {
 		        		try {
 			        		TeleporterUtils.teleport(entityIn, ServerLifecycleHooks.getCurrentServer().getLevel(dimension), blockData.getInt("dimX") + 2, blockData.getInt("dimY"), blockData.getInt("dimZ") + 2, entityIn.xRot,  entityIn.yRot);
 			        		System.out.println(mc.player.level.dimension().location().toString());
+			        		System.out.println(dimension.location().toString());
 		        		} catch (Exception e) {
 							e.printStackTrace();
 						}
@@ -54,5 +61,14 @@ public class PortalBlock extends Block {
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
     }
-
+    
+    @Override
+    public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult raytrace) {
+		Data blockData = GreenPortals.getInstance().blocks.getData(mc.getLevelSource().getBaseDir().toString(), pos);
+		mc.player.chat("X Coord > " + blockData.getInt("dimX"));
+		mc.player.chat("Y Coord > " + blockData.getInt("dimY"));
+		mc.player.chat("Z Coord > " + blockData.getInt("dimZ"));
+		mc.player.chat("D Coord > " + blockData.getString("dimId"));	
+    	return super.use(state, world, pos, player, hand, raytrace);
+    }
 }
