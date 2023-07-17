c730861f09c415d3a95cbad9173d0504c01686a5
Azures04 <gilleslazure04@gmail.com>
2023-07-17 04:13:03 +0200

    new commit about save point device

--- src/main/java/fr/azures/mod/greenportals/registry/ModItems.java
+++ src/main/java/fr/azures/mod/greenportals/registry/ModItems.java
@@ -2,6 +2,7 @@ package fr.azures.mod.greenportals.registry;
 
 import fr.azures.mod.greenportals.registry.items.NeutralizerGun;
 import fr.azures.mod.greenportals.registry.items.PortalGun;
+import fr.azures.mod.greenportals.registry.items.SavePointDevice;
 import fr.azures.mod.greenportals.utils.Constants;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -12,6 +13,6 @@ public class ModItems {
 	public static final DeferredRegister<Item> ITEMS =  DeferredRegister.create(ForgeRegistries.ITEMS, Constants.MOD_ID);
 	public static final RegistryObject<Item> PORTAL_GUN = ITEMS.register("portal_gun", () -> new PortalGun(new Item.Properties()));
 	public static final RegistryObject<Item> BLOCK_NEUTRALIZER_GUN = ITEMS.register("neutralizer_gun", () -> new NeutralizerGun(new Item.Properties()));
-	public static final RegistryObject<Item> SAVE_POINT_DEVICE = ITEMS.register("save_point_device", () -> new NeutralizerGun(new Item.Properties()));
+	public static final RegistryObject<Item> SAVE_POINT_DEVICE = ITEMS.register("save_point_device", () -> new SavePointDevice(new Item.Properties()));
 
 }
